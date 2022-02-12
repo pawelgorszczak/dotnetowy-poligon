c15f73cf9b0d95776f2874e33e8153c2b19a77c8
pawelgorszczak <pawelgorszczakpl@gmail.com>
2022-02-12 20:00:18 +0100

    memory management - Excercise2 static class

--- kill-house/.net/GC-Fundamenals/understanding-memory-managemnt/understatning-memory-managemnt/Exercises/Excercise2.cs
+++ kill-house/.net/GC-Fundamenals/understanding-memory-managemnt/understatning-memory-managemnt/Exercises/Excercise2.cs
@@ -1,15 +1,33 @@
+using System.Collections.Generic;
 using understatning_memory_managemnt.Models;
 
 namespace understatning_memory_managemnt
 {
     public class Exercise2
     {
-        /// Single Instance
+        /// Static class example with explicit reference
         public void Function1(){
             var classA = new ClassA();
+            var classA2 = new ClassA();
+            var classA3 = new ClassA();
 
             //operation
             //operation
+
+            Exercise2_Static_Class.ClassAStore.Add(classA2);
+
+            Sample_Method();
+
+            void Sample_Method(){
+                var classA4 = new ClassA();
+                Exercise2_Static_Class.ClassAStore.Add(classA4);
+            }
         }
+    }
+
+
+    public static class Exercise2_Static_Class
+    {
+        public static List<ClassA> ClassAStore = new List<ClassA>();
     }
 }
